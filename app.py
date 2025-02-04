d5a6e7f35c8e2b5068f450c0cbc2cde16e234d42
EverDCA <Evdacar18@gmail.com>
2025-02-04 16:16:10 -0500

    Create app.py
    Creacion de los ejercicios 1 y 2

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask
+
+
+#Ejercicio 1
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "¡Bienvenido a Flask!"
+
+#Ejercicio 2
+
+#2.1
+@app.route('/saludo/<nombre>')
+def saludo(nombre):
+    return f"Hola, {nombre}"
+
+#2.2
+@app.route('/suma/<int:num1>/<int:num2>')
+def suma(num1, num2):
+    return (num1 + num2)
+
+#2.3
+@app.route('/consulta/<string:palabra>')
+def consulta(palabra):
+    return palabra.upper()
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5050, host='127.0.0.1')
+    
+    
+    
+
